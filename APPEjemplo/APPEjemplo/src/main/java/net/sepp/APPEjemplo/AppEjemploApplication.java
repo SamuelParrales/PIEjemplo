b8d9870fe938003980883e571c05af7981fda14d
SamuelParrales <68031754+SamuelParrales@users.noreply.github.com>
2020-07-14 15:58:22 -0500

    Add files via upload

---
+++ APPEjemplo/APPEjemplo/src/main/java/net/sepp/APPEjemplo/AppEjemploApplication.java
@@ -0,0 +1,13 @@
+package net.sepp.APPEjemplo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppEjemploApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppEjemploApplication.class, args);
+	}
+
+}
---
+++ APPEjemplo/APPEjemplo/src/test/java/net/sepp/APPEjemplo/AppEjemploApplicationTests.java
@@ -0,0 +1,13 @@
+package net.sepp.APPEjemplo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppEjemploApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
