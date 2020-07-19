80ecb9c2debce954a49bad9b32e2ad83ebc5b260
Luis Dominguez <Luis Dominguez@DESKTOP-UH6B8ER.mshome.net>
2020-07-19 18:07:46 -0500

    Borrando comentario

--- APPEjemplo/APPEjemplo/src/main/java/net/sepp/repository/RecetaRepo.java
+++ APPEjemplo/APPEjemplo/src/main/java/net/sepp/repository/RecetaRepo.java
@@ -6,7 +6,7 @@ import org.springframework.data.repository.CrudRepository;
 
 import net.sepp.APPEjemplo.entities.Receta;
 
-/*Sirve como conexion a la base de datos*/
+
 public interface RecetaRepo extends CrudRepository <Receta,Long>{
 	List<Receta> findByNombre(String nombre);
 	List<Receta> findByPreparacion(String nombre);
