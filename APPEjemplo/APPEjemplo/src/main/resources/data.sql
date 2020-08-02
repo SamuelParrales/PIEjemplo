1cfd8c128d53b134dde54a52c3caf0be62cd932f
SamuelParrales <68031754+SamuelParrales@users.noreply.github.com>
2020-08-02 14:43:10 -0500

    Cambio de html a jsp

---
+++ APPEjemplo/APPEjemplo/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+insert into recetas(nombre,foto,preparacion,dificultad)
+values('Arroz','7f5cdd28c77b4199944febe905e7ac61','texto textot','facil'),
+('asas','7f5cdd28c77b4199944febe905e7ac61','texto textot','facil'),
+('Aasas','7f5cdd28c77b4199944febe905e7ac61','texto textot','facil'),
+('aawaz','7f5cdd28c77b4199944febe905e7ac61','texto textot','facil');
